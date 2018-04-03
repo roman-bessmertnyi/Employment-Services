3831297b9804b12196af6be421ee7a8f3d3ce40c
roman-bessmertnyi <roman.bezsmertniy@gmail.com>
2018-04-03 11:12:18 +0300

    Spring Initializr project created

---
+++ employment-services/src/main/java/com/romanbessmertnyi/employmentservices/EmploymentServicesApplication.java
@@ -0,0 +1,12 @@
+package com.romanbessmertnyi.employmentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentServicesApplication.class, args);
+	}
+}
