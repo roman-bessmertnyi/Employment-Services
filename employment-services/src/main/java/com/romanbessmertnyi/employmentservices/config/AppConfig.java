9be326a772ce3655ed505cc844d2cbf87d2f1b98
roman-bessmertnyi <roman.bezsmertniy@gmail.com>
2018-05-08 17:29:49 +0300

    frontend integration

--- employment-services/src/main/java/com/romanbessmertnyi/employmentservices/config/AppConfig.java
+++ employment-services/src/main/java/com/romanbessmertnyi/employmentservices/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -29,5 +30,10 @@ public class AppConfig implements WebMvcConfigurer{
 	@Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(roleToUserTypeConverter);
+    }
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
